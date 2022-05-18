c9a34c3370a6cadf006cbfaa215b5e0a530f15b0
RAFAPASKY <99282489+RAFAPASKY@users.noreply.github.com>
2022-05-18 08:37:54 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,26 +1,24 @@
 basic.showIcon(IconNames.Happy)
 let DISTANCIA = 0
-maqueen.servoRun(maqueen.Servos.S1, 0)
+maqueen.servoRun(maqueen.Servos.S1, 10)
 let strip = neopixel.create(DigitalPin.P15, 4, NeoPixelMode.RGB)
 basic.forever(function () {
     DISTANCIA = maqueen.Ultrasonic(PingUnit.Centimeters)
-})
-basic.forever(function () {
     if (DISTANCIA < 5) {
+        music.playMelody("C5 B A G F E D C ", 500)
         basic.showIcon(IconNames.Heart)
         basic.showString("HOLA")
-        basic.showString("ENCANTADO")
-        music.playMelody("C5 B A G F E D C ", 500)
         for (let index = 0; index < 2; index++) {
             maqueen.servoRun(maqueen.Servos.S1, 90)
-            strip.showRainbow(1, 360)
+            strip.showColor(neopixel.colors(NeoPixelColors.Green))
             basic.pause(1000)
-            maqueen.servoRun(maqueen.Servos.S1, 0)
-            strip.showRainbow(1, 360)
+            maqueen.servoRun(maqueen.Servos.S1, 10)
+            strip.showColor(neopixel.colors(NeoPixelColors.Red))
             basic.pause(1000)
         }
     } else {
         basic.showIcon(IconNames.Happy)
-        maqueen.servoRun(maqueen.Servos.S1, 0)
+        maqueen.servoRun(maqueen.Servos.S1, 10)
+        strip.showColor(neopixel.colors(NeoPixelColors.White))
     }
 })
